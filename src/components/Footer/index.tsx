340b323530173eaa534c7a4eecee71016feb984f
baiwumm <baiwumm@foxmail.com>
2023-12-15 13:40:14 +0800

    feat(Footer): 添加 ICP 备案图标

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,12 +3,13 @@
  * @Version: 2.0
  * @Author: 白雾茫茫丶
  * @Date: 2023-11-01 08:55:11
- * @LastEditors: 白雾茫茫丶
- * @LastEditTime: 2023-11-14 13:46:50
+ * @LastEditors: 白雾茫茫丶<baiwumm.com>
+ * @LastEditTime: 2023-12-15 09:43:32
  */
 import { CopyrightOutlined, GithubOutlined } from '@ant-design/icons'
 import { Row, Space, Typography } from 'antd'
 import { FC } from 'react'
+import IcpImage from '@/assets/icp.png'
 
 const { Text } = Typography;
 
@@ -20,8 +21,9 @@ const Footer: FC = () => {
         <a href="https://github.com/baiwumm" target="_blank" style={{ marginLeft: 10 }}>
           <Text type="secondary"><GithubOutlined style={{ marginRight: 5 }} />白雾茫茫丶</Text>
         </a>
+        |
         <a href="https://beian.miit.gov.cn/" target="_blank" title="粤ICP备2023007649号-2">
-          <img src="https://cdn.baiwumm.com/blog/images/icp.png!baiwu" alt="粤ICP备2023007649号-2" style={{ width: 16, marginRight: 5 }} />
+          <img src={IcpImage} alt="粤ICP备2023007649号-2" style={{ width: 16, marginRight: 5 }} />
           <Text type="secondary">粤ICP备2023007649号-2</Text>
         </a>
       </Space>
